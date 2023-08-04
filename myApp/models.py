723ce33faff898dfda478a22bec1ec38f6eed0e9
mbuguaa <mbuguajoys@gmail.com>
2023-08-04 10:45:14 +0300

    initial commit

---
+++ myApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Member(models.Model):
+    firstname=models.CharField(max_length=200)
+    lastname=models.CharField(max_length=200)
+    username=models.CharField(max_length=200)
+    password=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.firstname+''+self.lastname
+# Create your models here.
